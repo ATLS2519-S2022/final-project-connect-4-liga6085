b3340cc5e5319a290d2d146a74d6f95c8ea8d817
liga6085 <70085102+liga6085@users.noreply.github.com>
2022-04-29 20:29:27 -0600

    Create GreedyPlayer.java

---
+++ src/GreedyPlayer.java
@@ -0,0 +1,30 @@
+
+public class GreedyPlayer {
+
+	public void calcMove(Connect4Board board, int oppMoveCol, Arbitrator arb) throws TimeUpException{
+		if (board.isFull()) {
+			throw new Error ("Board full.");
+		}
+		
+		int[] scoreOpp = new int[7];
+		int max = -1000;
+		int colOpp = 0;
+		
+		for (int i = 0;i < cols; i++) {
+			if (board.isValidMove(i)) {
+				board.move(i,  3-id);
+				scoreOpp[i] = calcScore(board, 3-id);
+				board.unmove(i,  3-id);
+				
+				if(scoreOpp[i] > max) {
+					max = scoreOpp[i];
+					colOpp = i;
+				}
+			}
+		}
+		
+		arb.setMove(colOpp);
+	}
+	
+	
+}
