0b75eb7b5297f17532a75c6c315f3c0d3462d98a
liga6085 <70085102+liga6085@users.noreply.github.com>
2022-04-29 20:40:01 -0600

    Update MiniMax.java

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -97,8 +97,50 @@ public class MiniMax implements Player{
     	
 	}
 
-	private int calcScore(Connect4Board board, int id2) {
-		// TODO Auto-generated method stub
-		return 0;
+	private int calcScore(Connect4Board board, int id) {
+		
+		final int rows = board.numRows();
+		final int cols = board.numCols();
+		int score = 0;
+		// Look for horizontal connect-4s.
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c <= cols - 4; c++) {
+				if (board.get(r, c + 0) != id) continue;
+				if (board.get(r, c + 1) != id) continue;
+				if (board.get(r, c + 2) != id) continue;
+				if (board.get(r, c + 3) != id) continue;
+				score++;
+			}
+		}
+		// Look for vertical connect-4s.
+		for (int c = 0; c < cols; c++) {
+			for (int r = 0; r <= rows - 4; r++) {
+				if (board.get(r + 0, c) != id) continue;
+				if (board.get(r + 1, c) != id) continue;
+				if (board.get(r + 2, c) != id) continue;
+				if (board.get(r + 3, c) != id) continue;
+				score++;
+			}
+		}
+		// Look for diagonal connect-4s.
+		for (int c = 0; c <= cols - 4; c++) {
+			for (int r = 0; r <= rows - 4; r++) {
+				if (board.get(r + 0, c + 0) != id) continue;
+				if (board.get(r + 1, c + 1) != id) continue;
+				if (board.get(r + 2, c + 2) != id) continue;
+				if (board.get(r + 3, c + 3) != id) continue;
+				score++;
+			}
+		}
+		for (int c = 0; c <= cols - 4; c++) {
+			for (int r = rows - 1; r >= 4 - 1; r--) {
+				if (board.get(r - 0, c + 0) != id) continue;
+				if (board.get(r - 1, c + 1) != id) continue;
+				if (board.get(r - 2, c + 2) != id) continue;
+				if (board.get(r - 3, c + 3) != id) continue;
+				score++;
+			}
+		}
+		return score;
 	}
 }
