3b06f4db2b33aff0a0f613d2331ace850e42ef58
liga6085 <70085102+liga6085@users.noreply.github.com>
2022-04-29 23:36:31 -0600

    Update MiniMax.java

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,73 +1,47 @@
 
 public class MiniMax implements Player{
-
-	
 	int id;
 	int colNum;
 	int oppId;
-	
-	public String name() {
+	public String name() { //name of player
 		return "Mini Max";
 	}
-	
-	public void init(int id, int msecPerMove, int rows, int cols) {
-		this.id = id;
+	public void init(int id, int msecPerMove, int rows, int cols) { //intializing player
+		this.id = id; //player id
 		this.oppId = 3-id;
 		this.colNum = cols;
 	}
-
-	@Override
-	public void calcMove(Connect4Board board, int oppMoveCol, Arbitrator arb) throws TimeUpException {
+	public void calcMove(Connect4Board board, int oppMoveCol, Arbitrator arb) throws TimeUpException { //calling program to figure out next move, is there space to move?
 		
-		if (board.isFull()) {
+		if (board.isFull()) { //checking if board is full
 			throw new Error ("Board full.");
 		}
-		
 		int move = 0;
 		int depth = 1;
-		
-		while (!arb.isTimeUp() && depth <= board.numEmptyCells()) {
-			
-			int topScore = -1000; 
-            
-        	int [] keptScore = new int[7]; 
-            
+		while (!arb.isTimeUp() && depth <= board.numEmptyCells()) { //while there is time and space left on the board
+			int topScore = -1000; //highest value
+        	int [] keptScore = new int[7]; //temporarily stores scores in array
             int colTopScore = 0;
-            
-            
-            
-            
-            for (int i = 0 ; i < colNum ; i++) { 
-
-       
-            	if (board.isValidMove(i)) { 
-            		
+            for (int i = 0 ; i < colNum ; i++) {  //runs through rows and columns to find highest score
+            	if (board.isValidMove(i)) { //is the move valid?
     	        	board.move(i, id); 
-    	     
     	        	keptScore[i] = minimax(board, depth - 1, false, arb);
     	            board.unmove(i, id);
-                
                 if (keptScore[i] > topScore) {
-                	
                 	topScore = keptScore[i];
     	            colTopScore = i; 
-                
                 }
 		}
         }
             arb.setMove(colTopScore); 
             depth++;
 	}
-	
 }
-
 	private int minimax(Connect4Board board, int depth, boolean maximize, Arbitrator arb) {
 		int topScore;
-    	
     	if(depth == 0 || board.isFull() || arb.isTimeUp()) {
     		return calcScore(board, id) - calcScore(board, oppId);
     	}
-    	
     	if(maximize) {
     		topScore = -1000;
     		for (int i = 0 ; i < colNum ; i++) {
@@ -79,7 +53,6 @@ public class MiniMax implements Player{
     		}
     		 return topScore;
     	}
-    		
     	else {
     		topScore = 1000;
     		for (int i = 0 ; i < colNum ; i++) {
@@ -87,18 +60,13 @@ public class MiniMax implements Player{
     			board.move(i, oppId);
     			topScore = Math.min(topScore, minimax(board, depth - 1, true, arb));
     			board.unmove(i, oppId);
-    			
     			}
     			}
-    			
-    			return topScore;
-    			
+    			return topScore;	
     		}
-    	
 	}
 
 	private int calcScore(Connect4Board board, int id) {
-		
 		final int rows = board.numRows();
 		final int cols = board.numCols();
 		int score = 0;
