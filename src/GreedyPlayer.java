3b57b87f02c69da5dc0167830bc79eacb0026d00
liga6085 <70085102+liga6085@users.noreply.github.com>
2022-04-29 20:33:34 -0600

    Update GreedyPlayer.java

--- src/GreedyPlayer.java
+++ src/GreedyPlayer.java
@@ -5,24 +5,20 @@ public class GreedyPlayer {
 		if (board.isFull()) {
 			throw new Error ("Board full.");
 		}
-		
 		int[] scoreOpp = new int[7];
 		int max = -1000;
 		int colOpp = 0;
-		
 		for (int i = 0;i < cols; i++) {
 			if (board.isValidMove(i)) {
 				board.move(i,  3-id);
 				scoreOpp[i] = calcScore(board, 3-id);
 				board.unmove(i,  3-id);
-				
 				if(scoreOpp[i] > max) {
 					max = scoreOpp[i];
 					colOpp = i;
 				}
 			}
 		}
-		
 		arb.setMove(colOpp);
 	}
 	
