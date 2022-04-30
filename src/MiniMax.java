073953d770448357442fdb0ff05da25cff0bca33
liga6085 <70085102+liga6085@users.noreply.github.com>
2022-04-29 20:29:30 -0600

    Update MiniMax.java

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,4 +1,68 @@
 
-public class MiniMax {
+public class MiniMax implements Player{
 
+	
+	int id;
+	int colNum;
+	int oppId;
+	
+	public String name() {
+		return "Mini Max";
+	}
+	
+	public void init(int id, int msecPerMove, int rows, int cols) {
+		this.id = id;
+		this.oppId = 3-id;
+		this.colNum = cols;
+	}
+
+	@Override
+	public void calcMove(Connect4Board board, int oppMoveCol, Arbitrator arb) throws TimeUpException {
+		
+		if (board.isFull()) {
+			throw new Error ("Board full.");
+		}
+		
+		int move = 0;
+		int depth = 1;
+		
+		while (!arb.isTimeUp() && depth <= board.numEmptyCells()) {
+			
+			int bestScore = -1000; //stores highest value
+            
+        	int [] scoreKeep = new int[7]; //array that stores temporary scores to eventually see which score is the highest
+            
+            int colHighestScore = 0;
+            
+            
+            // Find maximum score for all possible moves 
+            
+            for (int i = 0 ; i < colNum ; i++) { //Runs through each column and finds the highest score if there is a possible move
+
+       
+            	if (board.isValidMove(i)) { 
+            		
+    	        	board.move(i, id); 
+    	     
+    	        	scoreKeep[i] = minimax(board, depth - 1, false, arb);
+    	            board.unmove(i, id);
+                
+                if (scoreKeep[i] > bestScore) {
+                	
+                	bestScore = scoreKeep[i];
+    	            colHighestScore = i; 
+                
+                }
+		}
+        }
+            arb.setMove(colHighestScore); 
+            depth++;
+	}
+	
+}
+
+	private int minimax(Connect4Board board, int i, boolean b, Arbitrator arb) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
